37a2ef0121b3292ceade2c53d1f8e51ac3a1029f
mwangisam203 <mwangisam203@gmail.com>
2026-05-02 03:06:07 -0400

    mod main

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ app.mount("/media", StaticFiles(directory="media"), name="media")
 
 templates = Jinja2Templates(directory="templates")
 
-app.include_router(users.router, prefix="api/users", tags=["users"])
-app.include_router(posts.router, prefix="api/posts", tags=["posts"])
+app.include_router(users.router, prefix="/api/users", tags=["users"])
+app.include_router(posts.router, prefix="/api/posts", tags=["posts"])
 
 
 
@@ -125,7 +125,6 @@ async def general_http_exception_handler(request: Request, exception: StarletteH
         },
         status_code=exception.status_code,
     )
-
 
 
 ## RequestValidationError Handler ***
