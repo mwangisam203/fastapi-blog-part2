10030a1c40509c2c8562b8b6dafee5da16310470
mwangisam203 <mwangisam203@gmail.com>
2026-05-03 05:06:11 -0400

    modified some files

--- main.py
+++ main.py
@@ -105,11 +105,10 @@ async def user_posts_page(
 @app.get("/login", include_in_schema=False)
 async def login_page(request: Request):
     return templates.TemplateResponse(
-        request,
-        "login.html",
-        {"title": "Login"},
+        request=request,
+        name="login.html",
+        context={"title": "Login"},
     )
-
 
 @app.get("/register", include_in_schema=False)
 async def register_page(request: Request):
